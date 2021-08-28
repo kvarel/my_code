0ec087d245ec8f3d002a24383661d122d811dbcc
kvarel <61622068+kvarel@users.noreply.github.com>
2021-08-28 16:26:13 +0300

    Update Rope.py

--- Rope.py
+++ Rope.py
@@ -1,1 +1,32 @@
 class Node:
+    
+    def __init__(self, key):
+        self.key = key
+        self.r = None
+        self.l = None
+        self.p = None
+        self.h = 1
+        
+    def __repr__(self):
+        return f'key {self.key}, r {self.r}, l {self.l}, sum {self.sum}'
+    
+class Rope:
+    
+    def find(self, number, node):
+        if node is None:
+            return None
+        if node.h == 1 and number == 1:
+            return node
+        if node.l:
+            if number - node.l.h == 1:
+                return node
+            if number - node.l.h > 1:
+                return Rope.find(number-node.l.h-1, node.r)
+            if number - node.l.h < 1:
+                return Rope.find(number, node.l)
+        else:
+            return node if number < node.h else Rope.find(number-1, node.r)
+    
+            
+                
+    
