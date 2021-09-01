ea255d9c75bd2f049c92c1e3765b610b84736864
kvarel <61622068+kvarel@users.noreply.github.com>
2021-09-01 21:25:46 +0300

    Update Rope.py

--- Rope.py
+++ Rope.py
@@ -5,36 +5,31 @@ class Node:
         self.r = None
         self.l = None
         self.h = 1
+        self.s = 1
 
     def __str__(self):
-        return f'[[[key {self.key} | l {self.l} | r {self.r} | h {self.h}]]]'
+        return f'[key: {self.key}, l: {self.l}, r: {self.r}, h: {self.h}, s: {self.s}]'
+
 
 class Rope:
 
     @staticmethod
-    def find(number, node):
-        if node is None:
-            return None
-        if node.h == 1 and number == 1:
-            return node
-        if node.l:
-            if number - node.l.h == 1:
-                return node
-            if number - node.l.h > 1:
-                return Rope.find(number - node.l.h - 1, node.r)
-            if number - node.l.h < 1:
-                return Rope.find(number, node.l)
-        else:
-            return node if number < node.h else Rope.find(number - 1, node.r)
-
-    @staticmethod
-    def h_update(*nodes):
+    def hs_update(*nodes):
         for node in nodes:
-            node.h = max([Rope.h(node.l), Rope.h(node.r)]) + 1
+            node.s = Rope.s(node.l) + Rope.s(node.r) + 1
+            node.h = max(Rope.h(node.l), Rope.h(node.r)) + 1
 
     @staticmethod
     def h(node):
         return node.h if node else 0
+
+    @staticmethod
+    def s(node):
+        return node.s if node else 0
+
+    @staticmethod
+    def key(node):
+        return str(node.key) if node else ''
 
     @staticmethod
     def add(key, node):
@@ -42,7 +37,7 @@ class Rope:
             return Node(key)
         else:
             node.r = Rope.add(key, node.r)
-            Rope.h_update(node)
+            Rope.hs_update(node)
             return Rope.balans(node)
 
     @staticmethod
@@ -76,60 +71,133 @@ class Rope:
         node.l = top.r
         top.r = node
         return top
-    
+
     @staticmethod
     def balans(node):
         while Rope.h(node.r) - Rope.h(node.l) > 1:
             if Rope.h(node.r.r) > Rope.h(node.r.l):
                 node = Rope.balans_l(node)
-                Rope.h_update(node.l, node)
+                Rope.hs_update(node.l, node)
                 return node
             else:
                 node = Rope.big_balans_l(node)
-                Rope.h_update(node.r, node.l, node)
+                Rope.hs_update(node.r, node.l, node)
                 return node
         while Rope.h(node.l) - Rope.h(node.r) > 1:
             if Rope.h(node.l.l) > Rope.h(node.l.r):
                 node = Rope.balans_r(node)
-                Rope.h_update(node.r, node)
+                Rope.hs_update(node.r, node)
                 return node
             else:
                 node = Rope.big_balans_r(node)
-                Rope.h_update(node.r, node.l, node)
+                Rope.hs_update(node.r, node.l, node)
                 return node
         else:
             return node
-        
+
     @staticmethod
     def split(number, node):
         if node == None:
-            return None
-        if number == node.h+1:
-            v1 = node.l
-            node.l = None
-            return (v1, node)
-        if number > node.l.h:
-            v = node.r
-            merge(v, v2)
-            node.r = None
-            return (node, Rope.split(number-Rope.h(node.l)-1))
-        if number < node.l.h:
-            v = node.l
-            merge(v, v1)
-            node.l = None
-            return (node, Rope.split(number-Rope.h(node.r)-1))
-            
-            
-            
+            return (None, None)
+        if number < Rope.s(node.l) + 1:
+            (v1, v2) = Rope.split(number, node.l)
+            V = Rope.Rope_merge_with_root(v2, node.r, node)
+            return (v1, V)
+        else:
+            (v1, v2) = Rope.split(number - Rope.s(node.l) - 1, node.r)
+            V = Rope.Rope_merge_with_root(node.l, v1, node)
+            return (V, v2)
+
+    @staticmethod
+    def merge_with_root(v1, v2, T):
+        T.l = v1
+        T.r = v2
+        return T
+
     @staticmethod
     def merge(v1, v2):
-        pass
-        
-        
+        if v1 is None:
+            return v2
+        if v2 is None:
+            return v1
+        T = Rope.max(v1)
+        v1 = Rope.delete(v1)
+        return Rope.Rope_merge_with_root(v1, v2, T)
 
-node = Node(7)
-node = Rope.add(10, node)
-node = Rope.add(15, node)
-node = Rope.add(20, node)
-node = Rope.add(25, node)
-print(node)
+    @staticmethod
+    def max(node):
+        if node.r:
+            return Rope.max(node.r)
+        else:
+            return node
+
+    @staticmethod
+    def delete(node):
+        if node.r:
+            node.r = Rope.delete(node.r)
+            Rope.hs_update(node)
+            return node
+        else:
+            return None if node.l is None else node.l
+
+    @staticmethod
+    def Rope_merge_with_root(v1, v2, T):
+        if abs(Rope.h(v1) - Rope.h(v2)) <= 1:
+            Rope.merge_with_root(v1, v2, T)
+            Rope.hs_update(T)
+            return T
+        else:
+            if Rope.h(v1) > Rope.h(v2):
+                t = Rope.Rope_merge_with_root(v1.r, v2, T)
+                v1.r = t
+                Rope.hs_update(v1.r, v1)
+                return Rope.balans(v1)
+            else:
+                t = Rope.Rope_merge_with_root(v1, v2.l, T)
+                v2.l = t
+                Rope.hs_update(v2.l, v2)
+                return Rope.balans(v2)
+
+    @staticmethod
+    def rope(i, j, k, node):
+        if k == i:
+            return node
+        v1, v2 = Rope.split(i, node)
+        v2, v3 = Rope.split(j-i+1, v2)
+        if k == 0:
+            v1 = Rope.merge(v2, v1)
+            node = Rope.merge(v1, v3)
+            return node
+        if k < i:
+            v4, v5 = Rope.split(k, v1)
+            v1 = Rope.merge(v4, v2)
+            v1 = Rope.merge(v1, v5)
+            node = Rope.merge(v1, v3)
+            return node
+        elif k > i:
+            v4,v5 = Rope.split(k-i, v3)
+            v3 = Rope.merge(v4, v2)
+            v3 = Rope.merge(v3, v5)
+            node = Rope.merge(v1,v3)
+            return node
+    @staticmethod
+    def rez(node):
+        if node is None:
+            return ''
+        if node.l == node.r == None:
+            return Rope.key(node)
+        else:
+            return Rope.rez(node.l) + Rope.key(node) + Rope.rez(node.r)
+
+def main():
+    string = input()
+    node = None
+    for i in string:
+        node = Rope.add(i, node)
+    for i in range(int(input())):
+        z, x, c = map(int, input().split())
+        node = Rope.rope(z, x, c, node)
+    print(Rope.rez(node))
+
+if __name__ == '__main__':
+    main()
