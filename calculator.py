213a07ffc1f1a5d4eb98d501be8cb278bbf44206
kvarel <61622068+kvarel@users.noreply.github.com>
2021-09-10 18:05:33 +0300

    Update calculator.py

--- calculator.py
+++ calculator.py
@@ -1,19 +1,31 @@
 def main(string):
+    global i
     stack = ['x', 'x']
-    string = string.split()
-    for i in string:
-        if i.isdigit():
+    while i < len(string):
+        if string[i].isdigit():
             if str(stack[-2]).isdigit() and stack[-1] in '*/':
                 y = stack.pop()
                 x = stack.pop()
-                stack.append(x*int(i) if y=='*' else x/int(i))
-            else: stack.append(int(i))
-        if i in '+-*/':
-            stack.append(i)
+                stack.append(x*int(string[i]) if y=='*' else x/int(string[i]))
+            else: stack.append(int(string[i]))
+        elif string[i] in '+-*/':
+            stack.append(string[i])
+        elif string[i] == '(':
+            i += 1
+            z = main(string[i:])
+            if str(stack[-2]).isdigit() and stack[-1] in '*/':
+                y = stack.pop()
+                x = stack.pop()
+                stack.append(x*z if y=='*' else x/z)
+            else: stack.append(int(string[i]))
+        elif string[i] == ')':
+            i += 1
+            break
+        i += 1
     while len(stack) > 3:
-        x = stack.pop()
+        x = int(stack.pop())
         y = stack.pop()
-        z = stack.pop()
+        z = int(stack.pop())
         stack.append(z+x if y == '+' else z-x)
     return(stack[2])
     
@@ -29,4 +41,6 @@ def test():
         
 
 if __name__ == '__main__':
-    print(main('4 / 2 + 18 - 2 * 6'))
+    i = 0
+    string = input().split()
+    print(main(string))
