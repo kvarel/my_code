3d5256f526944664c5bc04160eab3db653f6c355
kvarel <61622068+kvarel@users.noreply.github.com>
2021-09-11 12:52:12 +0300

    Update calculator.py

--- calculator.py
+++ calculator.py
@@ -12,7 +12,7 @@ def main(string):
             stack.append(string[i])
         elif string[i] == '(':
             i += 1
-            z = main(string[i:])
+            z = main(string)
             if str(stack[-2]).isdigit() and stack[-1] in '*/':
                 y = stack.pop()
                 x = stack.pop()
@@ -28,17 +28,7 @@ def main(string):
         z = int(stack.pop())
         stack.append(z+x if y == '+' else z-x)
     return(stack[2])
-    
-def test():
-    from random import randint as r, choice as c
-    for i in range(100):
-        x = r(1, 100)
-        y = r(1, 100)
-        b = c('-')
-        z = c(['*', '/'])
-        print(x, b, x, z, y)
-        assert main(f'{str(x)} {b} {str(x)} {z} {str(y)}') == x-x*y if z == '*' else x-x/y
-        
+
 
 if __name__ == '__main__':
     i = 0
